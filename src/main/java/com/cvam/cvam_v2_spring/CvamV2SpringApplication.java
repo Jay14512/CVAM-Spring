170b1a86f50eb7bf13ee637ada2c6849a2df9eea
Joshua Jason <jjm40971@gmail.com>
2026-08-01 18:36:19 +0200

    first commit after Spring setup

---
+++ src/main/java/com/cvam/cvam_v2_spring/CvamV2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.cvam.cvam_v2_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvamV2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvamV2SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvam/cvam_v2_spring/CvamV2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvam.cvam_v2_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvamV2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
